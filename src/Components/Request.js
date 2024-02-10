250d7f5ee191b278054c783a923b36eef1a63166
Mayur Desale <mdesale99@gmail.com>
2024-02-10 10:54:38 +0530

    new commit

--- src/Components/Request.js
+++ src/Components/Request.js
@@ -4,19 +4,19 @@ import "./Request.css";
 const Request = ( {}) => {
     const data = [
         {
-          reqid: 1,studname:'Mayur' ,dateandtime:'2024-02-09 10:00Am',roomno:  '101',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> | <img src="images/Tik.png"  width="20" /> </div>,
+          reqid: 1,studname:'Mayur' ,dateandtime:'2024-02-09 10:00Am',roomno:  '101',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> Allocate | <img src="images/Tik.png"  width="20" /> Completed </div>,
         },
         {
-          reqid: 2,studname:'Maneeesha' ,dateandtime:'2024-02-09 10:00AM',roomno:  '102', floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'not completed', allocation: <div> <img src="images/Plus.png"  width="20" /> | <img src="images/Tik.png"  width="20" /> </div>,
+          reqid: 2,studname:'Maneeesha' ,dateandtime:'2024-02-09 10:00AM',roomno:  '102', floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'not completed', allocation: <div> <img src="images/Plus.png"  width="20" /> Allocate | <img src="images/Tik.png"  width="20" /> Completed </div>,
           },
           {
-            reqid: 3,studname:'Ketan' ,dateandtime:'2024-02-09 10:00Am',roomno:  '103', floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> | <img src="images/Tik.png"  width="20" /> </div>,
+            reqid: 3,studname:'Ketan' ,dateandtime:'2024-02-09 10:00Am',roomno:  '103', floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> Allocate | <img src="images/Tik.png"  width="20" /> Completed </div>,
           },
           {
-            reqid: 4,studname:'Dhanush' ,dateandtime:'2024-02-09 10:00AM',roomno:  '104',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'pending', allocation: <div> <img src="images/Plus.png"  width="20" /> | <img src="images/Tik.png"  width="20" /> </div>,
+            reqid: 4,studname:'Dhanush' ,dateandtime:'2024-02-09 10:00AM',roomno:  '104',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'pending', allocation: <div> <img src="images/Plus.png"  width="20" /> Allocate | <img src="images/Tik.png"  width="20" /> Completed </div>,
           },
           {
-            reqid: 5,studname:'Rahul' ,dateandtime:'2024-02-09 10:00Am',roomno:  '105',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> | <img src="images/Tik.png"  width="20" /> </div>,
+            reqid: 5,studname:'Rahul' ,dateandtime:'2024-02-09 10:00Am',roomno:  '105',floorno:  '1', reqs: 'Cleaning, Mopping, Dusting.', status: 'completed', allocation: <div> <img src="images/Plus.png"  width="20" /> Allocate | <img src="images/Tik.png"  width="20" /> Completed </div>,
           },
         
       ];
